34bef91ebc616d6058166fde83cf678be3bd352d
dak0rn <daniel@suitsoft.eu>
2015-04-06 20:23:45 +0200

    Business logic

---
+++ app.js
@@ -0,0 +1,183 @@
+/**
+ * dak0rn.github.com
+ *
+ * Awesome SPA stuff
+ */
+(function(Backbone, Marionette, $, _){
+
+    var Post = Backbone.Model.extend({
+
+        defaults: {
+            title: '',
+            contents: '',
+            date: '',
+            basename: ''
+        },
+
+        url: function() {
+            return 'release/articles/' + this.get('basename') + '.html';
+        },
+
+        loadPost: function() {
+
+            return $.get( this.url() )
+                .done( _.bind(function(html) {
+                    this.set('contents', html);
+                }, this));
+
+        }
+
+    });
+
+    var Posts = Backbone.Collection.extend({
+        model: Post,
+        comparator: function(model1, model2) {
+            return model1.get('date') < model2.get('date');
+        }
+    });
+
+    var ArticleView = Marionette.ItemView.extend({
+        tagName: 'article',
+        template: _.template('<div class="article-title"><a href="#post"><%=title%></a></div><div class="article-content"><%=contents%></div>'),
+        triggers: {
+            'click .article-title>a': 'show:single'
+        },
+
+        onRender: function() {
+            this.$('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        }
+
+    });
+
+    var ArticlesView = Marionette.CollectionView.extend({
+        tagName: 'section',
+        childView: ArticleView
+    });
+
+    var app = new Marionette.Application();
+    app.layout = new Marionette.LayoutView({
+        el: 'body',
+
+        regions: {
+            'main': 'main',
+            'header': 'header'
+        }
+    });
+
+    var headerView = new (Marionette.ItemView.extend({
+        template: _.template('<a href="https://65535th.com">65535th</a>'),
+
+        events: {
+            'click a': 'home'
+        },
+
+        home: function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            app.router.navigate('');
+            app.trigger('show:index');
+        }
+    }))();
+
+    var NotFoundView = Marionette.ItemView.extend({
+        template: _.template('<div class="nf-title">Not found</div><div class="nf-text">The requested page could not be found</div><img src="arrow.png" id="home-arrow" />'),
+        tagName: 'div',
+        className: 'nf'
+    });
+
+    var ControllerClass = Marionette.Object.extend({
+
+        collection: null,
+
+        showIndex: function() {
+
+            $.get('release/posts.json')
+                .done( _.bind(function(object) {
+
+                    var posts = _.values(object);
+                    this.collection = new Posts(posts);
+                    var promises = [];
+
+                    this.collection.each( function(post) {
+                        promises.push( post.loadPost() );
+                    });
+
+                    $.when.apply($, promises)
+                        .then( _.bind(function() {
+                            var cview = new ArticlesView({collection: this.collection});
+
+                            this.listenTo(cview, 'childview:show:single', _.bind(function(view, args){
+                                var post = args.model.get('basename');
+                                app.router.navigate('post/'+post);
+                                app.trigger('show:post', post );
+                            },this));
+                            app.layout.getRegion('main').show(cview);
+                        }, this));
+                }, this));
+        },
+
+        showPost: function(what) {
+            var post = null;
+            var view = null;
+
+            if( _.isObject(this.collection) )
+                post = this.collection.where( {basename: what} );
+
+            if( _.isArray(post) && post.length > 0 ) {
+                view = new ArticleView({model: post[0]});
+                app.layout.getRegion('main').show( view );
+                $('body').scrollTop(0);
+            }
+            else {
+                $.get('release/posts.json')
+                    .done( _.bind(function(posts){
+                        var t = posts[what];
+
+                        if( _.isObject(t) ) {
+                            var model = new Post(t);
+                            model.loadPost().done(function(){
+                                view = new ArticleView({model:model});
+                                app.layout.getRegion('main').show( view );
+                            });
+
+                        }
+                        else {
+                            app.layout.getRegion('main').show( new NotFoundView() );
+                        }
+
+
+
+                    },this) );
+            }
+
+
+        }
+
+    });
+
+    var controller = new ControllerClass();
+
+    app.router = new Marionette.AppRouter({
+        controller: controller,
+
+        appRoutes: {
+            '': 'showIndex',
+            'post/:basename': 'showPost'
+        }
+    });
+
+    app.on('show:post', controller.showPost, controller);
+    app.on('show:index', controller.showIndex, controller);
+
+
+    app.on('start', function() {
+        Backbone.history.start();
+
+        app.layout.getRegion('header').show(headerView);
+    });
+
+    app.start();
+
+})(Backbone, Marionette, $, _);
